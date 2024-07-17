811497835f02538ad52f3d6dc2696cb0e1a0af89
Arthur Patsanovskiy <arthur2050@mail.ru>
2024-07-17 17:04:23 +0300

    Added the changes for the first endpoint.

--- src/Helper/ApiRequestInterface.php
+++ src/Helper/ApiRequestInterface.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @author    Arthur Patsanovskiy <arthur2050@mail.ru>
- * @copyright Copyright (c) 2024, Darvin Digital
- * @link      https://darvindigital.ru/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace App\Helper;
 
